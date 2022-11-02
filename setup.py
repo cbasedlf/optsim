c9dfb1bf0a4024b06dd092b70cda2d78b90876a9
F. Soldevila <soltfern@gmail.com>
2022-11-02 17:57:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="optsim",
-    version="0.0.1",
+    version="1.0.0",
     author="F. Soldevila",
     author_email="soltfern@gmail.com",
     description="Tools for optical simulations",
